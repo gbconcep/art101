9a7c06105c4620991b9959aa52d4e0b43033ea23
Gavin Concepcion <gbconcep@ucsc.edu>
2022-04-18 23:07:36 -0700

    Lab 4
    New Lab 4 Javascript and HTML files

---
+++ lab4/js/lab.js
@@ -0,0 +1,19 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   Lab 4 JS,Task 3, pseudocode computer task.
+// @author CassieHicks. Gavin Concepcion
+// @since  04.18.2022.
+
+//Creating a math calculation program:
+//Create area for user to input two numbers
+  //Code in places where the user will input
+  //Link them created GUIs
+//Create options for whether the user wants to add, subtract, multiply, or divide their numbers
+  //Put each of the options under a list with selectable inputs
+//Make “if” statements based on which option user selects
+//Code in a Calculate button
+  //Create a GUI for it
+//Test to see if program works properly
+  //Try it using all four different variations
+  //If something is off, go back and fix it and then test it again.
