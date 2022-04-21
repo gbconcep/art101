d4ec49b13e7ea70a81c0473233016736a4cef3e2
Gavin Concepcion <gbconcep@ucsc.edu>
2022-04-20 20:43:54 -0700

    Lab 5
    Javascript and HTML files for Lab 5 of ART 101

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Gavin Concepcion <gbconcep@ucsc.edu>
+ * Created: 04/20/2022
+ * License: Public Domain
+ */
+
+// Define Variables
+make = "Honda";
+model = "Civic";
+color = "Gray";
+year = 1997;
+ownIt = true;
+
+// calculate
+age = 2022 - 1997;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Own It: " + ownIt + "<br>");
