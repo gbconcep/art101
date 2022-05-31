b845c56b5d36bd9bf1bf3a1d54756269d1c25cc1
Gavin Concepcion <gbconcep@ucsc.edu>
2022-05-30 20:30:56 -0700

    Lab 16 Work
    Work-in-progress on Lab 16

---
+++ lab16/js/lab16.js
@@ -0,0 +1,31 @@
+/*
+ * Author: Gavin Concepcion <gbconcep@ucsc.edu>
+ * Created: 05/25/2022
+ * License: Public Domain
+ */
+
+var actButton = document.getElementById("activate")
+
+actButton.addEventListener("click", function(){
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://xkcd.com/json.html",
+    // The data to send (will be converted to a query string)
+    // Whether this is a POST or GET request
+    type: "GET",
+  })
+    // The type of data we expect back
+    .done(function(data){
+      console.log("Worked!")
+      console.log(JSON.stringify(data));
+
+      $("#output").append("<h1>" + data.answer + "</h1>");
+      $("#output").append("<img src=" + data.img + ">");
+      $("#output").append("<p>" + data.alt + "</p>");
+    })
+
+    .fail(function(request, error){
+    	$("#output").html("Something broke");
+    })
+  })
