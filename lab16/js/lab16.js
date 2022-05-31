9cacbfac99aa0a90eae89548488380ade8cba427
Gavin Concepcion <gbconcep@ucsc.edu>
2022-05-30 20:49:00 -0700

    Lab 16 Complete
    Completed version of Lab 16

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -1,6 +1,6 @@
 /*
  * Author: Gavin Concepcion <gbconcep@ucsc.edu>
- * Created: 05/25/2022
+ * Created: 05/30/2022
  * License: Public Domain
  */
 
@@ -10,7 +10,7 @@ actButton.addEventListener("click", function(){
 // Using the core $.ajax() method
 $.ajax({
     // The URL for the request (from the api docs)
-    url: "https://xkcd.com/json.html",
+    url: "https://xkcd.com/info.0.json",
     // The data to send (will be converted to a query string)
     // Whether this is a POST or GET request
     type: "GET",
@@ -20,7 +20,7 @@ $.ajax({
       console.log("Worked!")
       console.log(JSON.stringify(data));
 
-      $("#output").append("<h1>" + data.answer + "</h1>");
+      $("#output").append("<h1>" + data.title + "</h1>");
       $("#output").append("<img src=" + data.img + ">");
       $("#output").append("<p>" + data.alt + "</p>");
     })
